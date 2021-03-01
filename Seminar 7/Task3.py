d4018c1950a07e6a0a559820f4d60d1c7118db71
Narek Engibaryan <60289837+narek01@users.noreply.github.com>
2021-03-02 01:07:33 +0300

    Sem7_upd

--- Seminar 7/Task3.py
+++ Seminar 7/Task3.py
@@ -6,10 +6,13 @@ from scipy.stats.mstats import gmean
 
 df = pd.read_csv("TCGA-COAD_cancer_normal.tsv", sep="\t", index_col=0)
 df = df.loc[df.min(axis=1) > 0]
-df = df.assign(Pseudo=np.zeros(df.shape[0]))
-df.Pseudo = gmean(df.iloc[:,:-1], axis=1)
-df = df.div(df.Pseudo, axis=0).iloc[:,:-1].melt(var_name="Sample", value_name="Ratio")
+df = df.assign(Ps_sample=np.zeros(df.shape[0]))
+
+df.Ps_sample = gmean(df.iloc[:,:-1], axis=1)
+
+df = df.div(df.Ps_sample, axis=0).iloc[:,:-1].melt(var_name="Sample", value_name="Ratio")
 
 sns.barplot(x="Sample", y="Ratio", data=df, estimator=np.median)
+plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
